9589428fece8c5bae5d7fa6201c6953dff36e5b9
“Daniyal-sirpi” <Daniyal@sirpi.io>
2022-11-13 18:23:18 +0530

    cluster from atlas

--- db/connect.py
+++ db/connect.py
@@ -1,6 +1,6 @@
 import pymongo
 print("Welcome")
-client=pymongo.MongoClient("mongodb://localhost:27017")
+client=pymongo.MongoClient("mongodb+srv://dannymongo:nrkXZB2c1PEL0TfG@cluster0.oq7u4bt.mongodb.net/test") 
 print(client)
 db=client['todo']
 collections=db['todocollection']
