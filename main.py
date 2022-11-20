77276c8178f7de249f920ea8881174814aa14492
dannysyed <dannysyed20@gmail.com>
2022-11-20 05:20:33 -0800

    cors

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ from fastapi.security import HTTPBearer
 from utils import VerifyToken
 import http.client
 from db.connect import collections
-app = FastAPI()    
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
 token_auth_bearer=HTTPBearer()
 note_data=[]
 @app.get("/")
